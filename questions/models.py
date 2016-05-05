559058600eee9b6e8736ee15c354973c965d0c83
Agustin Muniz <gus.s.muniz@gmail.com>
2016-05-05 01:11:16 -0700

    initial creation of models

--- questions/models.py
+++ questions/models.py
@@ -2,4 +2,10 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+	name = models.CharField(max_length=100)
+
+class Question(models.Model):
+	text = models.TextField()
+	category = models.ForeignKey(Category, on_delete=models.CASCADE)
+	score = models.IntegerField()
