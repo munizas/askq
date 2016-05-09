bd0f63ec74f02172265ae8aeac9b0ae469af9519
Agustin Muniz <gus.s.muniz@gmail.com>
2016-05-09 00:36:01 -0700

    model updates and admin setup

--- questions/admin.py
+++ questions/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Question, Category
+
+admin.site.register(Question)
+admin.site.register(Category)
--- questions/models.py
+++ questions/models.py
@@ -1,11 +1,20 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 
+@python_2_unicode_compatible
 class Category(models.Model):
 	name = models.CharField(max_length=100)
 
+	def __str__(self):
+		return self.name
+
+@python_2_unicode_compatible
 class Question(models.Model):
 	text = models.TextField()
 	category = models.ForeignKey(Category, on_delete=models.CASCADE)
 	score = models.IntegerField()
+
+	def __str__(self):
+		return self.text
