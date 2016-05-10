0e51730179a8e22e9847c3e059266d50b3205e87
Agustin Muniz <gus.s.muniz@gmail.com>
2016-05-10 00:20:36 -0700

    bootstrap, views

--- questions/urls.py
+++ questions/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.categories, name='categories'),
+    url(r'^question/(?P<category>\w+)', views.question, name='question'),
 ]
--- questions/views.py
+++ questions/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import random
+from .models import Question
 
-def index(request):
-	return HttpResponse("Hello, world. you're at the questions index")
+def categories(request):
+	return render(request, 'categories.html')
+
+def question(request, category="Travel"):
+	questions = Question.objects.filter(category__name=category)
+	question = random.choice(questions)
+	context = {'content': question}
+	return render(request, 'question.html', context)
